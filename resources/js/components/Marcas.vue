6197a59080a2392cab660fb62577fde8c0eecb23
Pedro Esteves <pedroesteves2803@gmail.com>
2022-12-15 19:02:18 -0300

    implementando vue.

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <!-- inicio card de busca -->
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
+                        <div class="row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputID" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca">
+                                    <input type="number" class="form-control" id="inputID" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
+                            </div>
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="idHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="inputNome" placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </template>
+
+                    <template v-slot:rodape>
+
+                    </template>
+
+                </card-component>
+                <!-- fim card de busca -->
+
+                <!-- inicio card de busca -->
+                <card-component titulo="Relação de marcas">
+                    <template v-slot:conteudo>
+                        <table-component></table-component>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
+                <!-- fim card de busca -->
+            </div>
+        </div>
+
+        <modal-component id="modalMarca" titulo="Adicionar marca"></modal-component>
+
+    </div>
+</template>
+
+<script>
+
+</script>
